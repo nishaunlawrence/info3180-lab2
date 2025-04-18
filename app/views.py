5ab3cbc8642ba0ef7da98d4e67ba69ef49065235
nishaunlawrence <nishaun.lawrence@gmail.com>
2025-04-18 00:41:01 -0500

    Implemented profile page with improved styling and fixed navbar profile button

--- app/views.py
+++ app/views.py
@@ -1,45 +1,33 @@
 from app import app
 from flask import render_template, request, redirect, url_for, flash
-
-
-###
-# Routing for your application.
-###
+import datetime
 
 @app.route('/')
 def home():
     """Render website's home page."""
     return render_template('home.html')
 
-
 @app.route('/about/')
 def about():
     """Render the website's about page."""
     return render_template('about.html', name="Mary Jane")
 
+@app.route('/profile/')
+def profile():
+    """Render the user's profile page."""
+    date_joined = datetime.date(2019, 2, 7)  # Example date
+    formatted_date = format_date_joined(date_joined)
+    return render_template('profile.html', date_joined=formatted_date)
 
-###
-# The functions below should be applicable to all Flask apps.
-###
-
-@app.route('/<file_name>.txt')
-def send_text_file(file_name):
-    """Send your static text file."""
-    file_dot_text = file_name + '.txt'
-    return app.send_static_file(file_dot_text)
-
+def format_date_joined(date):
+    """Format the date as Month, Year."""
+    return date.strftime("%B, %Y")
 
 @app.after_request
 def add_header(response):
-    """
-    Add headers to both force latest IE rendering engine or Chrome Frame,
-    and also tell the browser not to cache the rendered page. If we wanted
-    to we could change max-age to 600 seconds which would be 10 minutes.
-    """
     response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
     response.headers['Cache-Control'] = 'public, max-age=0'
     return response
-
 
 @app.errorhandler(404)
 def page_not_found(error):
